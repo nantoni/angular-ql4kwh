5eb656874279f644ded5949cfad488a31d4ee620
nantoni <nathan.antoni@outlook.fr>
2019-10-30 13:39:46 +0100

    1 file modified

--- src/app/components/panier/panier.component.ts
+++ src/app/components/panier/panier.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngxs/store";
 import { PanierState } from "../../../../shared/states/panier-state";
 import { Produit } from "../../../../shared/models/produit";
+import { DelProduit } from "../../../../shared/actions/delProduit-action";
 
 @Component({
-  selector: 'app-panier',
-  templateUrl: './panier.component.html',
-  styleUrls: ['./panier.component.css']
+  selector: "app-panier",
+  templateUrl: "./panier.component.html",
+  styleUrls: ["./panier.component.css"]
 })
 export class PanierComponent implements OnInit {
-
   produits: Produit[] = [];
 
   constructor(private store: Store) {}
@@ -17,11 +17,23 @@ export class PanierComponent implements OnInit {
   ngOnInit() {
     this.store
       .select(state => state.panier.panier)
-      .subscribe(val => this.produits = val);
+      .subscribe(val => (this.produits = val));
   }
 
   log(val) {
     console.log(val);
   }
 
+  delProduit(produit: Produit) {
+    this.store.dispatch(
+      new DelProduit({
+        produit
+      })
+    );
+  }
+
+  onProduitRemoveClick(produit: Produit) {
+    this.delProduit(produit);
+    this.log(produit);
+  }
 }
