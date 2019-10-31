a601c89a41a4907d3ac25969efece1d2229d364d
nantoni <nathan.antoni@outlook.fr>
2019-10-31 10:15:09 +0100

    3 files modified

--- src/app/components/compte/compte.module.ts
+++ src/app/components/compte/compte.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CompteComponent } from './compte.component'
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProduitPipe } from "../../produit.pipe";
+import { ProduitService } from "../../produit.service";
+import { RouterModule, Routes } from '@angular/router';
+import { CompteComponent } from './compte.component';
+
+// Commit won't pass
+const routes: Routes = [
+  {
+    path: '',
+    component: CompteComponent
+  }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [CompteComponent]
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [CompteComponent],
+  exports: [RouterModule]
 })
-export class CompteModule { }
+export class CompteModule {}
--- src/app/components/panier/panier.module.ts
+++ src/app/components/panier/panier.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProduitPipe } from "../../produit.pipe";
+import { ProduitService } from "../../produit.service";
+import { RouterModule, Routes } from '@angular/router';
+import { PanierComponent } from './panier.component';
+
+// Commit won't pass
+const routes: Routes = [
+  {
+    path: '',
+    component: PanierComponent
+  }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [PanierComponent],
+  exports: [RouterModule]
 })
-export class PanierModule { }
+export class PanierModule {}
