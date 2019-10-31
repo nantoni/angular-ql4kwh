091dc0a3acb06ea7024109cf80a4c4852df2950b
Nathan ANTONI <nathan.antoni@outlook.fr>
2019-10-31 09:52:36 +0100

    Update liste.module.ts

--- src/app/components/liste/liste.module.ts
+++ src/app/components/liste/liste.module.ts
@@ -5,13 +5,18 @@ import { ProduitPipe } from "../../produit.pipe";
 import { ProduitService } from "../../produit.service";
 import { RouterModule, Routes } from '@angular/router';
 
-
+// Commit won't pass
+const routes: Routes = [
+  {
+    path: '',
+    component: ListeComponent
+  }
+];
 
 @NgModule({
-  imports: [CommonModule,
+  imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: [ListeComponent, ProduitPipe],
   providers: [ProduitService],
   exports: [RouterModule]
 })
 export class ListeModule {}
-
